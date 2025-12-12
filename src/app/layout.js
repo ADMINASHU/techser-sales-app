4dc2c8fde6a798665d5245e2c9549f309780bfa4
Ashutosh Kumar <kashutosh02@gmail.com>
2025-12-12 18:42:09 +0530

    feat: Implement root layout and integrate Vercel Analytics, Speed Insights, theme provider, and Knock client.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +32,8 @@ export default async function RootLayout({ children }) {
                         <Toaster />
                     </ThemeProvider>
                 </KnockClientProvider>
+                <SpeedInsights />
+                <Analytics />
             </body>
         </html>
     );
