3fa952b03dbc5e5bc35493180ae0da1c381018d9
Ashutosh Kumar <kashutosh02@gmail.com>
2026-01-06 16:15:10 +0530

    feat: add protected layout with authentication and profile setup checks

--- src/app/(protected)/layout.js
+++ src/app/(protected)/layout.js
@@ -3,7 +3,6 @@ import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import KnockClientProvider from "@/components/KnockClientProvider";
 
 export default async function ProtectedLayout({ children }) {
     const session = await auth();
@@ -21,18 +20,14 @@ export default async function ProtectedLayout({ children }) {
     if (!hasProfile && !isSetupPage) {
         redirect("/setup");
     }
-    
-
 
     return (
-        <KnockClientProvider session={session}>
-            <div className="min-h-screen bg-gray-50 dark:bg-gray-950 flex flex-col">
-                <Navbar />
-                <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 w-full grow">
-                    {children}
-                </main>
-                <Footer />
-            </div>
-        </KnockClientProvider>
+        <div className="min-h-screen bg-gray-50 dark:bg-gray-950 flex flex-col">
+            <Navbar />
+            <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 w-full grow">
+                {children}
+            </main>
+            <Footer />
+        </div>
     );
 }
